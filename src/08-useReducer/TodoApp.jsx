e7dc523dd08c981ca0bfe5d7c65a7141f9e688b0
Alondra Covarrubias <acovarrubias10@ucol.mx>
2023-09-20 13:08:30 -0600

    Añadiendo maquetado

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -11,7 +11,7 @@ const initialState = [
         id: new Date().getTime() * 3,
         description: 'Recolectar la piedra del alma',
         done: false,
-    }
+    },
 ]
 
 
@@ -21,14 +21,44 @@ export const TodoApp = () => {
   
     return (
     <>
-        <h1>TodoApp</h1>
+        <h1>TodoApp: 10 <small>pendientes: 2</small></h1>
         <hr />
 
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
+        <div className="row">
+            <div className="col-7">
+                <ul className="list-group">
+
+                    {
+                        todos.map( todo => (
+                            <li key={ todo.id } className="list-group-item d-flex justify-content-between">
+                                <span className="align-self-center">Item 1</span>
+                                <button className="btn btn-danger">Borrar</button>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+
+            <div className="col-5">
+                <h4>Agregar TODO</h4>
+                <hr />
+                <form>
+                    <input 
+                        type="text"
+                        placeholder="¿Qué hay que hacer?"
+                        className="form-control"
+                     />
+
+                     <button 
+                        type="submit"
+                        className="btn btn-outline-primary mt-1"
+                     >
+                        Agregar
+                     </button>
+                </form>
+            </div>
+        </div>
+
     </>
   )
 }
